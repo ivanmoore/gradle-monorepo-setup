f8ba7050437959fa8d5a36854068be4dc3a61705
Ivan Moore <ivan@teamoptimization.com>
2025-01-10 16:21:05 +0000

    "Creating a plugin that does nothing"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,11 @@
 //// settings.gradle.kts ////
 /////////////////////////////
 rootProject.name = "teashop"
+
+pluginManagement {
+    // Specifies an included build for defining custom plugins in the multi-project build.
+    includeBuild("gradle/plugins")
+}
+
 include("teasite")
+include("teapayments")
